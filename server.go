674cd0d69d4d8df5cc1025a09d899ed56cc979fe
sinan <sinangoo@gmx.de>
2016-09-27 22:55:05 +0200

    automatic commit

--- server.go
+++ server.go
@@ -214,6 +214,7 @@ func (s *Server) Handle(task Tasker, authorizers Authorizers, ec *EndpointConfig
 
 		// read request body
 		var body = ""
+    glog.Errorf("Bla:body:%s:", r.Body)
 		if r.Body != nil {
 			if b, err := ioutil.ReadAll(r.Body); err != nil {
 				body = string(b)
