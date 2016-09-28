31ec5950b390a8f70c4e3b2685212f55d14f3a48
Sinan Goo <sinan.goo@hypoport.de>
2016-09-28 23:18:58 +0200

    Added a usage example script

--- example_run.sh
+++ example_run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-./goexpose -config example/bash_tasks_config.json
+./goexpose -config example/bash_tasks_config.json &
+GOEXPOSE_PID="$!"
+
+sleep 2
+
+curl -i -X POST \
+-H "Content-Type: application/json" \
+-d '{"abc":4}' \
+"http://localhost:9900/body/bla?aaa=4"
+
+# curl "http://localhost:9900/"
+
+kill "$GOEXPOSE_PID"
+
+
